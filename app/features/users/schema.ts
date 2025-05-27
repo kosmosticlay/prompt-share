e085a34d73d446a618785a9702a276dcb72f9e69
Hyemin Clay Lee <kosmosticlay@gmail.com>
2025-05-28 01:13:02 +0900

    fix(prompts): Update key definition in table schema
    Refactors a key definition within the prompts database table schema.

--- app/features/users/schema.ts
+++ app/features/users/schema.ts
@@ -1,11 +1,4 @@
-import {
-  jsonb,
-  pgSchema,
-  pgTable,
-  text,
-  timestamp,
-  uuid,
-} from "drizzle-orm/pg-core";
+import { pgSchema, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 /* Supabase 연결할 때 지울 예정*/
 export const users = pgSchema("auth").table("users", {
@@ -21,7 +14,6 @@ export const profiles = pgTable("profiles", {
   email: text().notNull().unique(), // 프로필 이메일
   avatar_url: text(), // 프로필 아바타 이미지 주소
   bio: text(), // 프로필 자기소개
-
   created_at: timestamp().notNull().defaultNow(), // 프로필 생성 시간
   updated_at: timestamp().notNull().defaultNow(), // 프로필 수정 시간
 });
--- app/migrations/0000_freezing_yellow_claw.sql
+++ app/migrations/0000_freezing_yellow_claw.sql
@@ -18,14 +18,10 @@ CREATE TABLE "profiles" (
 	"email" text NOT NULL,
 	"avatar_url" text,
 	"bio" text,
-	"stats" jsonb,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "profiles_email_unique" UNIQUE("email")
 );
---> statement-breakpoint
-CREATE TABLE "auth"."users" (
-	"id" uuid PRIMARY KEY NOT NULL
-);
+
 --> statement-breakpoint
 ALTER TABLE "profiles" ADD CONSTRAINT "profiles_profile_id_users_id_fk" FOREIGN KEY ("profile_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;
